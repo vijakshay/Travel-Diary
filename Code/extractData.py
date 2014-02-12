d599d4edb83ce2ff8500c3e9240ab1d712c852e4
Andrew510 <twowheelmotion@gmail.com>
2014-02-11 17:25:32 -0800

    Andrew's data: 02/07/14 and 02/10/14

--- Code/extractData.py
+++ Code/extractData.py
@@ -1,4 +1,4 @@
-import urllib2 
+import urllib2
 import csv
 import os
 
@@ -36,7 +36,7 @@ def nextDay(year, month, day):
             return year + 1, 1, 1
         else:
             return year, month + 1, 1
-        
+
 
 # Returns True if year1-month1-day1 is after year2-month2-day2.  Otherwise, returns False.
 
@@ -48,10 +48,10 @@ def dateIsAfter(year1, month1, day1, year2, month2, day2):
             return True
         if month1 == month2:
             return day1 > day2
-    return False        
+    return False
 
 
-# Returns the starting and ending epoch Time in milliseconds for a given day, as per PST. 
+# Returns the starting and ending epoch Time in milliseconds for a given day, as per PST.
 # Input must be a 8-character string containing a valid date in MMDDYYYY format.
 
 def epochTime(date, gmtConversion):
@@ -63,19 +63,19 @@ def epochTime(date, gmtConversion):
     year2 = int(date[4:8])
     month2 = int(date[0:2])
     day2 = int(date[2:4])
-    
+
     days = 0
     while dateIsAfter(year2, month2, day2, year1, month1, day1):
         days += 1
         (year1, month1, day1) = nextDay(year1, month1, day1)
-    
+
     startTime = ((days * 24) - gmtConversion) * 3600 * 1000
     endTime = startTime + (24 * 3600 * 1000)
     return startTime, endTime
 
 
-# Returns the epoch Time in milliseconds for a given day and time in UTC. Input is a string in 
-# the format 'Thu Jan 23 18:18:00 UTC 2014' 
+# Returns the epoch Time in milliseconds for a given day and time in UTC. Input is a string in
+# the format 'Thu Jan 23 18:18:00 UTC 2014'
 
 def epochGroundTime(dateTime):
 
@@ -95,28 +95,28 @@ def epochGroundTime(dateTime):
               'Oct': 10,
               'Nov': 11,
               'Dec': 12}
-                  
+
     dateTime = dateTime.split()
     year2 = int(dateTime[-1])
     month2 = int(months[dateTime[1]])
     day2 = int(dateTime[2])
-    
+
     days = 0
     while dateIsAfter(year2, month2, day2, year1, month1, day1):
         days += 1
         (year1, month1, day1) = nextDay(year1, month1, day1)
 
     time = dateTime[3].split(':')
-    hours = int(time[0]) 
+    hours = int(time[0])
     minutes = int(time[1])
     seconds = int(time[2])
-    
+
     epochTime = ((((((days * 24) + hours) * 60) + minutes) * 60) + seconds) * 1000
     return epochTime
 
 
-# Procedure that takes as input strings deonting the tester name, test phone number, date, difference between 
-# local time zone and UTC time, and the GPS file path name. Output is a list of lists containing GPS data 
+# Procedure that takes as input strings deonting the tester name, test phone number, date, difference between
+# local time zone and UTC time, and the GPS file path name. Output is a list of lists containing GPS data
 # for that tester, phone and day.
 
 def getGPSData(testerName, phoneNum, date, gmtConversion, gpsFilePath):
@@ -127,30 +127,30 @@ def getGPSData(testerName, phoneNum, date, gmtConversion, gpsFilePath):
         for row in csv.reader(csvfile, delimiter = '\t'):
             try:
                 if int(row[1]) >= startTime and int(row[1]) <= endTime:
-                    gpsData.append(row[:-1])            
+                    gpsData.append(row[:-1])
             except:
-                pass            
+                pass
     gpsData = sorted(gpsData, key = lambda x: int(x[1]))
     return gpsData
 
 
-# Procedure that takes as input strings deonting the tester name, test phone number, date, difference between 
-# local time zone and UTC time, and the file path name. Output is a list of lists containing ground truth data 
+# Procedure that takes as input strings deonting the tester name, test phone number, date, difference between
+# local time zone and UTC time, and the file path name. Output is a list of lists containing ground truth data
 # for that tester, phone and day.
 
 def getGroundData(testerName, phoneNum, date, gmtConversion, groundFilePath):
-    
+
     groundData = []
     with open(groundFilePath, 'rU') as csvfile:
         for row in csv.reader(csvfile, delimiter = ','):
-            if row[1] == phoneNum and row[4] == testerName: 
+            if row[1] == phoneNum and row[4] == testerName:
                 if row[-2] == 'Yes':
                     time = epochGroundTime(row[0])
-                else:                
+                else:
                     time = epochGroundTime(row[-1])
-                row.append(time)                
-                groundData.append(row[:])            
-    
+                row.append(time)
+                groundData.append(row[:])
+
     startTime, endTime = epochTime(date, gmtConversion)
     groundData = sorted(groundData, key = lambda x: x[-1])
     startIndex, endIndex = 0, len(groundData)-1
@@ -164,7 +164,7 @@ def getGroundData(testerName, phoneNum, date, gmtConversion, groundFilePath):
 
     return groundData[startIndex:endIndex+1]
 
-# Procedure that takes as input two lists, one containing GPS data and one containing ground truth, and combines them 
+# Procedure that takes as input two lists, one containing GPS data and one containing ground truth, and combines them
 # into a single list
 
 def mergeGPSWithGroundTruth(gpsData, groundData):
@@ -183,11 +183,11 @@ def mergeGPSWithGroundTruth(gpsData, groundData):
         if 'Other' in groundData[10]:
             gpsData.append(groundData[11])
         gpsData.append(groundData[12])
-    
+
     return gpsData
-        
-    
-# Procedure that takes as input the list of lists containing GPS data and the ground truth, and combines them 
+
+
+# Procedure that takes as input the list of lists containing GPS data and the ground truth, and combines them
 # into a single list of lists.
 
 def mergeData(gpsData, groundData):
@@ -211,23 +211,23 @@ def mergeData(gpsData, groundData):
 
 # A script that combines the GPS data with the ground truth in a tab-delimited text file that can
 # subsequently be used to train inference algorithms. The GPS data file must be saved manually
-# as a tab-delimited text file on the local hard drive. The ODK file containing ground truth must 
+# as a tab-delimited text file on the local hard drive. The ODK file containing ground truth must
 # be exported manually as a csv and saved to the local hard drive as well.
 
 # Personal details, change as appropriate
-testerName = 'Vij'       # Should be the same as that listed in the ODK form
-phoneNum = '5107250744'  # 10-digit number with no brackets, hyphens or spaces
-date = '01272014'        # MMDDYYYY format of day for which you wish to extract data
+testerName = 'Andrew'       # Should be the same as that listed in the ODK form
+phoneNum = '5107259365'  # 10-digit number with no brackets, hyphens or spaces
+date = '02072014'        # MMDDYYYY format of day for which you wish to extract data
 gmtConversion = -8       # Difference in hours between local time and UTC time, remember to change for daylight savings
 
 # Base directory where you clone the repository, change as appropriate
-filePath = '/Users/biogeme/Desktop/Vij/Academics/Post-Doc/' 
+filePath = '/Users/daddy30000/'
 
 # Directory where you saved the file with GPS traces, change as appropriate
-gpsFilePath = filePath + 'Travel-Diary/Data/Google Play API/' + phoneNum + '.txt'
+gpsFilePath = '/Users/daddy30000/Dropbox/Research/13_14_Project_Files/13_Tracking_Apps/logs/gaeandroid.txt'
 
 # Directory where you've saved the ODK file with just the ground truth, change as appropriate
-groundFilePath = filePath + 'Travel-Diary/Data/Ground Truth/Travel_and_Activity_Diary_results.csv'       
+groundFilePath = '/Users/daddy30000/Dropbox/Research/13_14_Project_Files/13_Tracking_Apps/logs/14_02_07_10_odk_edit.csv'
 
 
 # DON'T CHANGE ANYTHING BELOW THIS!
